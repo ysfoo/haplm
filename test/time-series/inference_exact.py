2a5ebb6cb52d68bc31a29d6fb2973d30acce2d6a
Yong See Foo <yongsee0819@yahoo.com>
2023-07-27 00:31:06 +1000

    streamline MCMC-Exact and MCMC-Approx scripts for time series

--- test/time-series/inference_exact.py
+++ test/time-series/inference_exact.py
@@ -1,45 +1,59 @@
-import matplotlib.pyplot as plt
+"""
+Perform MCMC-Exact on time series example.
+
+Input files required:
+- Observation time, pool size, and observed allele counts for each pool
+  @ '../../data/time-series/time_series.data'
+
+Output files produced:
+- InferenceData output of MCMC-Exact
+  @ '../../data/time-series/psize{pool_size}_m{n_markers}_id{ds_idx}_exact.netcdf'
+- Posterior predictive samples as a numpy.darray of dimensions (chains*N) x H
+  @ '../../data/time-series/psize{pool_size}_m{n_markers}_id{ds_idx}_exact_pred_samples.npy'
+- Summary statistics of posterior predictive distribution as a dictionary
+  @ '../../data/time-series/psize{pool_size}_m{n_markers}_id{ds_idx}_exact_sumstats.pkl'
+"""
+
 import numpy as np
 import pickle as pkl
 import pulp
 from time import time
 
 import pymc as pm
-import arviz as az
-import scipy.special
-import jax
-import jax.numpy as jnp
-import pytensor
+# import arviz as az
 import pytensor.tensor as pt
-from pytensor.graph import Apply, Op
-from pytensor.link.jax.dispatch import jax_funcify
 
 from haplm.lm_dist import LatentMult, find_4ti2_prefix
-from haplm.hap_util import num_to_str, str_to_num, mat_by_marker
+from haplm.hap_util import mat_by_marker
 from haplm.lm_inference import hier_latent_mult_mcmc
 from haplm.gp_util import GP
 
 import numpyro
 
-chains = 5
-tune = 1000
-draws = 1000
-numpyro.set_host_device_count(chains)
-
 # init for other libraries
 solver = pulp.apis.SCIP_CMD(msg=False)
 prefix_4ti2 = find_4ti2_prefix()
 
+# data parameters
 H = 8
 N = 30 # number of data points
 pool_size = 50
 n_markers = 3
+
+# MCMC parameters
+chains = 5
+cores = 5
+n_burnin = 1000
+n_sample = 1000
+numpyro.set_host_device_count(cores)
+
 amat = mat_by_marker(n_markers)
 
+# each row of input file consists of observation time, pool size, allele counts of each marker
 t_obs = []
 ns = []
 ys = []
-with open('../../data/time-series/time-series.data') as fp:
+with open('../../data/time-series/time_series.data') as fp:
     for line in fp:
         tokens = line.split()
         t_obs.append(float(tokens[0]))
@@ -47,6 +61,7 @@ with open('../../data/time-series/time-series.data') as fp:
         ys.append(np.array([int(x) for x in tokens[2:]]))
 t_obs = np.array(t_obs)
 
+# preprocessing
 t = time()
 lm_list = []
 for i, (n, y) in enumerate(zip(ns, ys)):    
@@ -54,8 +69,9 @@ for i, (n, y) in enumerate(zip(ns, ys)):
                     solver, prefix_4ti2, enum_sols=True)
     lm_list.append(lm)
 pre_time = time() - t
-print(pre_time, flush=True)
+# print(pre_time, flush=True)
 
+# define GP prior
 with pm.Model() as model:
     sigma = pm.InverseGamma('sigma', alpha=3, beta=1)
     alpha = pm.InverseGamma('alpha', alpha=3, beta=3, shape=H)
@@ -72,36 +88,36 @@ with pm.Model() as model:
 
 t = time()
 with model:
-    idata = hier_latent_mult_mcmc(ps, lm_list, H, draws, tune, ['exact']*N,
-                                  jaxify=False, chains=chains, seed=2023, postprocessing_chunks=25)
+    idata = hier_latent_mult_mcmc(ps, lm_list, H, n_sample, n_burnin, ['exact']*N, jaxify=False, 
+                                  chains=chains, random_seed=2023, postprocessing_chunks=25)
 mcmc_time = time() - t
 
 idata.sample_stats.attrs['preprocess_time'] = pre_time
 idata.sample_stats.attrs['mcmc_walltime'] = mcmc_time
 
-ess = az.ess(idata, var_names=['p'])['p'].values
-print(ess.min())
+# ess = az.ess(idata, var_names=['p'])['p'].values
+# print(ess.min())
 
 idata.posterior = idata.posterior.drop_vars('noise')
 idata.to_netcdf(f'../../data/time-series/psize50_m3_exact.netcdf')
 
+# simulate predictive distribution
 t_pred = np.arange(0, 20.001, 0.01)
 N_pred = len(t_pred)
 with model:
     fs_pred = [gps[h].marg_cond(f'f_pred{h}', Xnew=t_pred[:,None]) for h in range(H)]
-    ps_pred = pm.Deterministic(f'p_pred', pm.math.softmax(pt.stack(fs_pred, axis=1)
-                                               *alpha + mean
-                                               + sigma*pm.Normal('noise_pred', shape=(N_pred,H)),
-                                               axis=-1))
+    ps_pred = pm.Deterministic(f'p_pred', pm.math.softmax(pt.stack(fs_pred, axis=1)*alpha + mean
+                                                          + sigma*pm.Normal('noise_pred', shape=(N_pred,H)),
+                                                          axis=-1))
     pred_idata = pm.sample_posterior_predictive(idata, var_names=['p_pred'])
 
 pred_samples = np.vstack(pred_idata.posterior_predictive.p_pred)
 np.save(f'../../data/time-series/psize50_m3_exact_pred_samples.npy', pred_samples)
-np.save('../../data/time-series/psize50_m3_exact_pred_samples_tint.npy',
-pred_samples[:,::100])
+# np.save('../../data/time-series/psize50_m3_exact_pred_samples_tint.npy', pred_samples[:,::100])
 
 # pred_samples = np.load(f'../../data/time-series/psize50_m3_exact_pred_samples.npy')
 
+# summary statistics of predictive distribution
 sumstats = {}
 sumstats['mean'] = pred_samples.mean(axis=0)
 sumstats['sd'] = pred_samples.std(axis=0)
